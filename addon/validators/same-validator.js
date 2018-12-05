80b62cc8ff6cbc55d0f7d3951ba5a430bde52ad7
Kado <kadodamball@hotmail.com>
2018-12-05 15:38:01 +0100

    Default same validator value to null to avoid undefined
    Somehow, using default value on first param breaks on minification. So, it would only break on production level code :(

--- addon/validators/same-validator.js
+++ addon/validators/same-validator.js
@@ -3,8 +3,11 @@ import EmberObject, { get } from '@ember/object';
 export default EmberObject.extend({
   validate(value, validator) {
     let [fieldName] = get(validator, 'arguments');
+    let fieldValue = get(validator, `field:${fieldName}`);
+    let formValue = value !== undefined ? value : null;
+    let validatorValue = fieldValue !== undefined ? fieldValue : null;
 
-    if(value !== get(validator, `field:${fieldName}`)) {
+    if(formValue !== validatorValue) {
       return {
         message: 'mustBeSame',
         replacements: { fieldName }
