3bd5cd053067817b8f9e3e6564ecf545c85bde21
lean8086 <lean8086@gmail.com>
2019-08-21 11:03:52 +0200

    Move completely to ember-intl

--- tests/unit/utils/message-provider-test.js
+++ tests/unit/utils/message-provider-test.js
@@ -1,7 +1,6 @@
 import { set } from '@ember/object';
 import messageProvider from 'ember-legit-forms/utils/message-provider';
 import { module, test } from 'qunit';
-import Ember from 'ember';
 
 module('Unit | Utility | message provider');
 
@@ -16,45 +15,18 @@ test('it gets validation message', function(assert) {
   assert.equal(subject.getMessage('testKey'), 'test message');
 });
 
-test('it checks first i18n before getting local keys (globals)', function(assert) {
-  Ember.i18n = {
-    t() {
-      return 'test message from i18n globals';
-    }
-  };
-
-  assert.equal(subject.getMessage('testKey'), 'test message from i18n globals');
-});
-
-test('it checks first intl before i18n', function(assert) {
-  Ember.i18n = null;
+test('it checks first intl before getting local keys (globals)', function(assert) {
   set(subject, 'container', {
-    lookup(param) {
+    lookup() {
       return {
         t() {
-          return `test message from ${param}`;
+          return 'test message from intl service';
         }
       };
     }
   });
 
-  assert.equal(subject.getMessage('testKey'), 'test message from service:intl');
-});
-
-test('it checks i18n before getting local keys (globals)', function(assert) {
-  Ember.i18n = null;
-  set(subject, 'container', {
-    lookup(param) {
-      if (param === 'service:intl') { return; }
-      return {
-        t() {
-          return `test message from ${param}`;
-        }
-      };
-    }
-  });
-
-  assert.equal(subject.getMessage('testKey'), 'test message from service:i18n');
+  assert.equal(subject.getMessage('testKey'), 'test message from intl service');
 });
 
 test('it can interpolate messages with and w/o replacements', function(assert) {
@@ -69,21 +41,6 @@ test('it can interpolate messages with and w/o replacements', function(assert) {
 test('when intl installed but key not defined it uses default translation', function(assert) {
   set(subject, 'container', {
     lookup() {
-      return {
-        t(key) {
-          return `Missing translation: [${key}]`;
-        }
-      };
-    }
-  });
-  const message = subject._fetchMessage('testKey', {});
-  assert.equal(message, defaultMessages.testKey)
-});
-
-test('when i18n installed but key not defined it uses default translation', function(assert) {
-  set(subject, 'container', {
-    lookup(param) {
-      if (param === 'service:intl') { return; }
       return {
         t(key) {
           return `Missing translation: [${key}]`;
